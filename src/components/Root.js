59cb9f34527a9f68066cf4b7c02d07ed61616098
Mark Anderson <udacity.com@ma7.org>
2017-08-06 15:31:57 -0500

    feat: Add React Router

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,15 @@
+//https://egghead.io/lessons/javascript-redux-adding-react-router-to-the-project
+
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
 
 const Root = ({ store }) =>
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Provider>;
 
 export default Root;
